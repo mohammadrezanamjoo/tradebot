807891523797989592f8d4f618eeaca7bae04796
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2023-12-13 11:12:34 +0330

    Create trendcrypto.js

---
+++ trendcrypto.js
@@ -0,0 +1,34 @@
+async function findMostTrendingCrypto() {
+  const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets';
+  const params = {
+    vs_currency: 'usd',
+    order: 'gecko_desc',
+    per_page: 1,
+    page: 1,
+    sparkline: false,
+  };
+
+  try {
+    // Construct the URL with query parameters
+    const url = new URL(apiUrl);
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+    // Fetch data from the CoinGecko API
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // Check if any data is available
+    if (data.length > 0) {
+      const mostTrendingCrypto = data[0];
+      console.log('Most trending cryptocurrency:', mostTrendingCrypto.name);
+    } else {
+      console.log('No trending cryptocurrencies found.');
+    }
+
+  } catch (error) {
+    console.error('Error fetching cryptocurrency trends:', error);
+  }
+}
+
+// Call the function to find the most trending cryptocurrency
+findMostTrendingCrypto();
