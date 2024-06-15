d50b125b8ee316bcea94adcf35b6445e7fc68101
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-06-15 11:45:16 +0330

    Update moving average.js

--- moving average.js
+++ moving average.js
@@ -1,6 +1,6 @@
 function priceActionStrategy(prices, period1, period2) {
   if (!Array.isArray(prices)) {
-    throw new Error('Invalid input: prices must be an array');
+    throw new Error('Invalid input: prices  be an array');
   }
   if (!Number.isInteger(period1) || period1 <= 0 || !Number.isInteger(period2) || period2 <= 0) {
     throw new Error('Invalid input: period values must be positive ');
